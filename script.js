00f4561504926f12d68b8db2d23dadc406bcbc87
JeyKul <77121325+JeyKul@users.noreply.github.com>
2023-12-26 21:38:57 +0100

    Website: Changes
    Add 3 Roms with all info
    Add a "url" option to roms.json
    make image clickable
    make dark background
    make 1:1 picture force, (crop instead of squish)
    make gradiant animated

--- script.js
+++ script.js
@@ -1,68 +1,65 @@
 async function fetchRoms() {
     try {
-      const response = await fetch('roms.json');
-      return await response.json();
+        const response = await fetch('roms.json');
+        return await response.json();
     } catch (error) {
-      console.error('Error fetching ROMs:', error);
-      return [];
+        console.error('Error fetching ROMs:', error);
+        return [];
     }
-  }
-  
-  function displayRoms(roms) {
+}
+
+function createRomItem(rom) {
+    const romItem = document.createElement('div');
+    romItem.classList.add('fileItem');
+
+    const romLink = document.createElement('a');
+    romLink.href = rom.url;
+    romItem.appendChild(romLink);
+
+    const romImage = document.createElement('img');
+    romImage.src = rom.image;
+    romImage.alt = rom.name;
+    romLink.appendChild(romImage);
+
+    const romDetails = document.createElement('div');
+    romDetails.classList.add('romDetails');
+    romItem.appendChild(romDetails);
+
+    const romName = document.createElement('p');
+    romName.textContent = rom.name;
+    romDetails.appendChild(romName);
+
+    const lastUpdated = document.createElement('p');
+    lastUpdated.textContent = 'Last Updated:';
+    romDetails.appendChild(lastUpdated);
+
+    const dateValue = document.createElement('p');
+    dateValue.textContent = rom.lastUpdated;
+    romDetails.appendChild(dateValue);
+    
+    return romItem;
+}
+
+async function displayRoms() {
     const fileList = document.getElementById('fileList');
-    roms.sort((a, b) => new Date(b.lastUpdated) - new Date(a.lastUpdated));
-  
-    roms.forEach(rom => {
-      const romItem = document.createElement('div');
-      romItem.classList.add('fileItem');
-  
-      const romImage = document.createElement('img');
-      romImage.src = rom.image;
-      romImage.alt = rom.name;
-      romItem.appendChild(romImage);
-  
-      const romName = document.createElement('p');
-      romName.textContent = rom.name;
-      romItem.appendChild(romName);
-  
-      const lastUpdated = document.createElement('p');
-      lastUpdated.textContent = `Last Updated: ${rom.lastUpdated}`;
-      romItem.appendChild(lastUpdated);
-  
-      fileList.appendChild(romItem);
-    });
-  }
-  
-  function adjustColors(backgroundImageUrl) {
-    const colorThief = new window.ColorThief();
-    const img = new Image();
-    img.src = backgroundImageUrl;
-  
-    img.onload = function () {
-      const dominantColor = colorThief.getColor(img);
-  
-      document.body.style.backgroundColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
-  
-      const buttons = document.querySelectorAll('.button');
-      buttons.forEach(button => {
-        button.style.backgroundColor = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
-        button.style.color = '#fff';
-      });
-  
-      const contentText = document.querySelectorAll('#content, .fileItem p');
-      contentText.forEach(textElement => {
-        textElement.style.color = `rgb(${dominantColor[0]}, ${dominantColor[1]}, ${dominantColor[2]})`;
-      });
-    };
-  }
-  
-  document.addEventListener('DOMContentLoaded', async () => {
+    fileList.innerHTML = ''; // Clear the existing content
+
     try {
-      const roms = await fetchRoms();
-      displayRoms(roms);
-      adjustColors('Background.jpg');
+        const roms = await fetchRoms();
+        roms.forEach(rom => {
+            const romItem = createRomItem(rom);
+            fileList.appendChild(romItem);
+        });
     } catch (error) {
-      console.error('Error on DOMContentLoaded:', error);
+        console.error('Error displaying ROMs:', error);
     }
-  });
-  
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        await displayRoms();
+        adjustColors('Background.jpg');
+    } catch (error) {
+        console.error('Error on DOMContentLoaded:', error);
+    }
+});
